0986f584e734243c3733323c6b99ce6cec349e07
Juan Andres Lorenzo <juanandreslorenzo01@gmail.com>
2023-03-29 04:11:04 -0300

    afnd_to_afd

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # automatasFInitos
 Scripts de python para hacer conversiones entre Automatas FInitos Deterministas, No deterministas, No deterministas Epsilon y Expreciones Regulares.
+
+AFND --> AFD 
+    ejemplo:
+    ti = transiciones
+
+    delta || t1                | t2 | ... | tn
+    ===========================================
+    q0    || {q0,q1, ... , qj} | {qp} |   |{ql}
+    q1    || {qi}              | {qr} |   |
+    .     ||  .                |      |...|
+    .     ||  .                |      |   |
+    .     ||  .                |      |   |
+    qm    || {q0,q1, ... , qj} | {qt} |   | {qk}
+
+    El input que se debe ingresar debe ser de la siguiente forma:
+    [[{0,1}, {0}], [{2}, {2}], [{3}, {}], [{3}, {3}]]
+
+    se puede visualizar de mejor forma:
+        [
+            [{0,1}, {0}],
+            [{2}  , {2}],
+            [{3}  , {} ], 
+            [{3}  , {3}]
+        ]
---
+++ afnd_to_afd.py
@@ -0,0 +1,53 @@
+def crearFila(estados, AFND):
+    res = []
+    t=0
+    for estado in estados:
+        h=0
+        if(t==0):
+            res = AFND[estado].copy()
+        else:
+            while(h<len(AFND[estado])):
+                res[h] = res[h].union(AFND[estado][h])
+                h+=1
+        t=1
+    return res
+
+def isInSetList(inSet, setList):
+    for sets in setList:
+        if inSet == sets:
+            return True
+    return False
+    
+
+def afnd_to_afd(AFND):
+    existentes = [{0}]
+    AFD = []
+    AFD.append(crearFila({0}, AFND))
+    i=0
+    while(i<len(AFD)):
+        j=0
+        while(j<len(AFD[0])):
+            if not isInSetList(AFD[i][j], existentes):
+                existentes.append(AFD[i][j].copy())
+                AFD.append(crearFila(AFD[i][j], AFND))
+            j+=1
+        i+=1
+    return AFD
+
+afnd1 = input() 
+
+# fila = afnd_to_afd(afnd1)
+fila = afnd_to_afd(afnd1)
+
+print("fila: ")
+x=0
+while(x<len(fila)):  
+    print(fila[x])
+    x+=1
+
+# afnd1 = [[0, [0,1], 0], [1, 0, 0]]
+# res = afnd_to_afd(afnd1)
+# for x in res:
+#     for y in res[0]:
+#         print(res[x][y])
+
